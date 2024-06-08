0124a9d88aefd88b539da3339cd2391ad6b5609f
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-06-08 11:06:27 +0200

    Reinnitialize build

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, {
